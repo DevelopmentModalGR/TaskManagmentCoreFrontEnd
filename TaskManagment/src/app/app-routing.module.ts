0a6a3f955760da8848dfd9c88d601fe38c31d584
Gr4zi <grazi.silvaribeiro31@hotmail.com>
2021-02-02 15:51:51 -0300

    pagina de login feita, mas nao carregando

--- TaskManagment/src/app/app-routing.module.ts
+++ TaskManagment/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './Pages/home-page/home-page.component';
+import { LoginPageComponent } from './Pages/login-page/login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: '',
+  component: HomePageComponent,
+  children: [ 
+    { path:'login', component: LoginPageComponent}    
+  ]
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- TaskManagment/src/app/app.module.ts
+++ TaskManagment/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginPageComponent } from './Pages/login-page/login-page.component';
+import { HomePageComponent } from './Pages/home-page/home-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginPageComponent,
+    HomePageComponent,
   ],
   imports: [
     BrowserModule,
